0255d20f09f5038d253a13e85a7bc78314cd1418
Yehor Savchuk <95549799+YehorSk@users.noreply.github.com>
2024-04-30 18:55:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/movieapp/model/Result.java
@@ -0,0 +1,46 @@
+package com.example.movieapp.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Result {
+    private Integer page;
+    private Integer totalPages;
+    private Integer totalResults;
+    private List<Movie> results = null;
+
+    public Result() {
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public List<Movie> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Movie> results) {
+        this.results = results;
+    }
+}
